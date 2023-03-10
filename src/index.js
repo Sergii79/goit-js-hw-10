5ad9fca5b46d49526fa7ae179d97a386986a7380
Sergii79 <tsymbal.sergiy@gmail.com>
2023-03-10 17:44:12 +0200

    commit first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const r = fetch('https://pokeapi.co/api/v2/pokemon/2/');
+console.log(r);
+
+fetch('https://pokeapi.co/api/v2/pokemon/2/')
+    .then(response => {
+        return response.json();    
+    })
+    .then(pokemon => {
+        console.log(pokemon);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
