2ad6d769c3a9b9c98da4d062350c509ae98142f6
Sergii79 <110824315+Sergii79@users.noreply.github.com>
2023-03-16 20:55:14 +0000

    Deploying to gh-pages from @ Sergii79/goit-js-hw-10@8bc58852f6564a9e41bfa64387a5e43e5a977d5d 🚀

--- index.9c956ce3.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=fetch("https://pokeapi.co/api/v2/pokemon/2/");console.log(o),fetch("https://pokeapi.co/api/v2/pokemon/2/").then((function(o){return o.json()})).then((function(o){console.log(o)})).catch((function(o){console.log(o)}))}();
-//# sourceMappingURL=index.9c956ce3.js.map
