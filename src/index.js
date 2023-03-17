e6876eecb2040fe780da6fbf6f379e65cea46dc2
Sergii79 <tsymbal.sergiy@gmail.com>
2023-03-17 15:08:19 +0200

    commit third

--- src/index.js
+++ src/index.js
@@ -5,24 +5,9 @@ console.log(fetchCountries);
 
 import debounce from 'lodash.debounce';
 
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
-
-// const r = fetch('https://pokeapi.co/api/v2/pokemon/2/');
-// console.log(r);
-
-// fetch('https://pokeapi.co/api/v2/pokemon/2/')
-//     .then(response => {
-//         return response.json();    
-//     })
-//     .then(pokemon => {
-//         console.log(pokemon);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
-
-
 
 const inputElement = document.getElementById('search-box');
 const listElement = document.querySelector('.country-list');
@@ -42,6 +27,40 @@ function inputHandler(event) {
         return;
       
     }
+  
+    fetchCountries(textInput)
+    .then(response => {
+      if (response.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
+
+        resetSearch(listElement);
+        resetSearch(infoElement);
+
+        return;
+      }
+
+      if (response.length >= 2 && response.length <= 10) {
+        resetSearch(countryInfo);
+        setSearch(infoElement, countryListSearch(response));
+
+        return;
+      }
+
+      if (response.length === 1) {
+        resetSearch(listElement);
+        setSearch(infoElement, countryInfoSearch(response));
+
+        return;
+      }
+    })
+    .catch(() => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      resetSearch(listElement);
+      resetSearch(infoElement);
+    });
+  
 }
 
 function resetSearch(ref) {
@@ -50,8 +69,34 @@ function resetSearch(ref) {
   }
   return;
 }
-    
-// console.log(inputHandler);
-// const inputHandler = event => {
-
+// function resetSearch(ref) {
+//   return ref.innerHTML = '';
 // }
+   
+function setSearch(ref, search) {
+  ref.innerHTML = search;
+}
+
+function countryListSearch(countries) {
+  return countries
+    .map(country => {
+      return `<li><img src="${country.flags.svg}" alt="${country.name.common} flag" width="50" height="25"></img> <p>${country.name.official}</p>
+              </li>`;
+    })
+    .join('');
+}
+
+function countryInfoSearch(countries) {
+  return countries
+    .map(country => {
+      return `<img 
+    src="${country.flags.svg}" alt="${country.name.common}" 
+    width="50" height="25" />
+    <h1>${country.name.common}</h1>
+    <p><b>Capital:</b> ${country.capital}</p>
+    <p><b>Population:</b> ${country.population}</p>
+    <p><b>Languages:</b> ${Object.values(country.languages)}</p>`;
+    })
+    .join('');
+}
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,11 +2,11 @@ function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v3.1';
     const properties = ['name', 'capital', 'population', 'flags', 'languages'];
     return fetch(
-      `${BASE_URL}/name/${name}?fields=${properties.join(',')}`).then(response => {
+      `${BASE_URL}/name/${name}?fields=${properties.join(',')}`
+    ).then(response => {
       if (!response.ok) {
         throw new Error(response.statusText);
       }
-
       return response.json();
     });
 }
